fa4895d6c8c4a0c3b254ad704e66d3fc838ff891
Leonardo Venturini <leovenbag@gmail.com>
2023-01-07 11:53:00 -0400

    put resolve back

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export function isMemoryMongoAvailable() {
   try {
-    require('mongodb-memory-server')
+    require.resolve('mongodb-memory-server')
     return true
   } catch (e) {
     console.log('Mondle: Memory Database Disabled')
